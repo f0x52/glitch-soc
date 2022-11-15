1806edaa149a62d83223bf2ca6d527b8e8896bc9
f0x <f0x@cthu.lu>
2022-11-15 20:29:58 +0100

    toot toot mfer

--- app/javascript/flavours/glitch/features/compose/components/publisher.js
+++ app/javascript/flavours/glitch/features/compose/components/publisher.js
@@ -16,7 +16,7 @@ import { maxChars } from 'flavours/glitch/util/initial_state';
 //  Messages.
 const messages = defineMessages({
   publish: {
-    defaultMessage: 'Publish',
+    defaultMessage: 'Toot',
     id: 'compose_form.publish',
   },
   publishLoud: {
